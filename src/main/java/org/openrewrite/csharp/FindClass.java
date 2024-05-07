ecd912c4fc83e42edc16c27794c7b76612528a23
Knut Wannheden <knut@moderne.io>
2024-05-07 19:35:12 +0200

    Update C# application code
    Now includes demo Java recipe

---
+++ src/main/java/org/openrewrite/csharp/FindClass.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.csharp;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.config.OptionDescriptor;
+import org.openrewrite.config.RecipeDescriptor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.remote.Receiver;
+import org.openrewrite.remote.RemotingClient;
+import org.openrewrite.remote.Sender;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+@RequiredArgsConstructor
+public class FindClass extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Demo recipe finding Java class declarations";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Demo recipe finding any Java class declarations using a C# recipe.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<>() {
+            @Override
+            public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext ctx) {
+                return runRecipe(cu, ctx);
+            }
+        };
+    }
+
+    private J.CompilationUnit runRecipe(J.CompilationUnit document, ExecutionContext ctx) {
+        RemotingClient remotingClient = getRemotingClient(ctx);
+        return remotingClient.runRecipe(getRemoteDescriptor(), document, (senderContext, before) -> {
+            Sender<J> sender = senderContext.newSender(J.class);
+            sender.send(document, before);
+        }, receiverContext -> {
+            Receiver<J> receiver = receiverContext.newReceiver(J.class);
+            return (J.CompilationUnit) receiver.receive(document);
+        });
+    }
+
+    private RemotingClient getRemotingClient(ExecutionContext ctx) {
+        return RemotingClient.create(ctx, this, Paths.get("dotnet/Rewrite.Server.dll"));
+    }
+
+    private RecipeDescriptor getRemoteDescriptor() {
+        RecipeDescriptor descriptor = getDescriptor();
+        List<OptionDescriptor> options = descriptor.getOptions();
+        return new RecipeDescriptor("Rewrite.Java.FindClass", descriptor.getDisplayName(), descriptor.getDescription(), descriptor.getTags(),
+                descriptor.getEstimatedEffortPerOccurrence(), options, descriptor.getRecipeList(), descriptor.getDataTables(),
+                descriptor.getMaintainers(), descriptor.getContributors(), descriptor.getExamples(), descriptor.getSource());
+    }
+
+}
---
+++ src/test/java/org/openrewrite/csharp/FindClassTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.csharp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.SourceSpec;
+
+import static org.openrewrite.java.Assertions.java;
+
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
+class FindClassTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new FindClass()).cycles(1).expectedCyclesThatMakeChanges(1);
+    }
+
+    @Test
+    void simple() {
+        rewriteRun(
+          java(
+            """
+              class T {
+              }
+              """,
+            """
+              /*~~>*/class T {
+              }
+              """,
+            SourceSpec::noTrim
+          )
+        );
+    }
+}
